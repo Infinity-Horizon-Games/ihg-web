50fecf7d84b8f8fae9c7a672a1c3a0cad98d0536
DML <90888178+ddgaidi@users.noreply.github.com>
2024-11-21 00:25:14 +0100

    code push

--- src/routes/jeux/+page.svelte
+++ src/routes/jeux/+page.svelte
@@ -1,10 +1,10 @@
 <script>
-  import Navbar from '../lib/components/navbar/Navbar.svelte';
-  import "../app.css";
+  import Navbar from '../../lib/components/navbar/Navbar.svelte';
+  import "../../app.css";
 </script>
 
 <svelte:head>
-	<title>IHG - Accueil</title>
+	<title>IHG - Jeux</title>
 	<meta name="description" content="IHG !" />
 	<meta content="#006633" data-react-helmet="true" name="theme-color"/>
 </svelte:head>
